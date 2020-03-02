210fe788c86b58dbfce64a02c045ed0929733659
BinBin He <hebinsquared@gmail.com>
2020-03-02 15:26:14 -0600

    Refactor: Reorganized parsefile to it's own function.

--- report_parser.js
+++ report_parser.js
@@ -1,28 +1,29 @@
 import * as fs from 'fs';
 
-export default class ReportParser{
-    mergePath(dir, filename){
+export default class ReportParser {
+    mergePath(dir, filename) {
         return dir.concat(filename)
     }
 
-    parseFile(filename){
+    parseFile(filename) {
+        fs.readFile(filename, 'utf8', (err, contents)=>{
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(contents)
+            }
+
+        })
         return error, contents
     }
 
-    parse(config, callback){
-        let error, data = fs.readFile(config.filename,'utf8', (err, contents) =>{ 
-            if (err){
-                console.log(err)
-            }
-            else{
-                console.log(contents)
-            }
-            
-        })
+    parse(config, callback) {
+
+        this.parseFile(config.filename)
 
         console.log(error)
-        console.log(data )
-        return "nothing" 
+        console.log(data)
+        return "nothing"
     }
 
 }
