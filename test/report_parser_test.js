45268b8e07bc2f31c930fb2e798734a4c45df645
BinBin He <hebinsquared@gmail.com>
2020-03-02 15:00:34 -0600

    Feature Update: 1-Added tests for parse(), 2-Added one test for mergePath(), 3-Added const test configs.

--- test/report_parser_test.js
+++ test/report_parser_test.js
@@ -1,33 +1,54 @@
 // const ReportParser= require('../report_parser')
 import ReportParser from '../report_parser.js';
-import * as chai from 'chai';
+import chai from 'chai';
 
 let assert = chai.assert
+
+const default_parser_config = {
+    'working_dir': '/var/tmp/working',
+    'filename': 'qa.report',
+    'target_field': 'report_status'
+};
 
 const parser_config = {
     'working_dir': '/var/tmp/working',
     'filename': 'qa.report',
-    'target_field': 'report_status'
+    'target_field': 'report_status',
+    'remove_after_parse':'true'
 
-};
+}
+
+const error_string = "Incomplete inputs, check your inputs!"
 
 describe('Report Parser', function () {
 
-    it('nil inputs return error', function () {
-        let result = new ReportParser(parser_config, (err, value)=> {console.log(value)})
-
-        console.log(result)
+    it('nil inputs return error string', function () {
+        let report= new ReportParser
+        
+        let result = report.parse(parser_config, (err, value)=> {console.log(value)})
+        assert.equal(result, error_string)
     });
 
     it('optional remove_after_parse=false returns with file intact', function () {
-
     });
 
     it('optional remove_after_parse=true returns with file gone', function() {
-
     })
 
     it('valid inputs return with non-nil target field value', function() {
+        let report= new ReportParser
+        
+        let result = report.parse(parser_config, (err, value)=> {console.log(value)})
 
+        assert.isNotNaN(result, "Result is not nil.")
     })
 });
+
+
+describe('Directory merge', function(){
+    it('nil inputs return error string', function(){
+        let parser = new ReportParser
+        let result = ReportParser.mergePath("","")
+        assert.equal( result, error_string)
+    });
+})
