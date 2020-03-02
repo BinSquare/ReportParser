3a8eb4784966f838ab0aa180b8c68b54c108f17c
BinBin He <hebinsquared@gmail.com>
2020-03-02 14:58:39 -0600

    Feature Update + Refactor: Removed constructor, decoupling file path merging and fileParsing to sibling funcs.

--- report_parser.js
+++ report_parser.js
@@ -1,14 +1,32 @@
-export default class ReportParser{
-    constructor(dir, filename, field){
-        //checks valid inputs
+import * as fs from 'fs';
 
-        this._dir = dir;
-        this._filename= filename;
-        this._field = field
+export default class ReportParser{
+    mergePath(dir, filename){
+        return dir.concat(filename)
     }
 
+    parseFile(filename){
+
+
+        return error, contents
+    }
+
+
     parse(config, callback){
-        return callback(config)
+
+        let error, data = fs.readFile(config.filename,'utf8', (err, contents) =>{ 
+            if (err){
+                console.log(err)
+            }
+            else{
+                console.log(contents)
+            }
+            
+        })
+
+        console.log(error)
+        console.log(data )
+        return "nothing" 
     }
 
 }
