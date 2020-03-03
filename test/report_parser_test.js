74b167cb3d5bfc577ebf4acfb2cf646a356e9e11
BinBin He <hebinsquared@gmail.com>
2020-03-03 07:00:58 -0600

    Refactor: Ran beautify formatter.

--- test/report_parser_test.js
+++ test/report_parser_test.js
@@ -2,7 +2,9 @@
 import ReportParser from '../report_parser.js';
 import chai from 'chai';
 import fs from 'fs';
-import {promisify} from 'util'
+import {
+    promisify
+} from 'util'
 
 let assert = chai.assert
 
@@ -12,12 +14,18 @@ const default_parser_config = {
     'target_field': 'report_status'
 };
 
-const parser_config = {
+const parser_config_complete = {
     'working_dir': '/var/tmp/working',
     'filename': 'qa.report',
     'target_field': 'report_status',
-    'remove_after_parse':true
+    'remove_after_parse': true
+}
 
+const parser_config_incomplete = {
+    'working_dir': '',
+    'filename': '',
+    'target_field': '',
+    'remove_after_parse': true
 }
 
 const error_string = "Incomplete inputs, check your inputs!"
@@ -25,40 +33,44 @@ const error_string = "Incomplete inputs, check your inputs!"
 describe('Report Parser', function () {
 
     it('nil inputs return error string', function () {
-        let report= new ReportParser
-        
-        let result = report.parse(parser_config, (err, value)=> {console.log(value)})
+        let report = new ReportParser
+
+        let result = report.parse(parser_config_incomplete, (err, value) => {
+            console.log(value)
+        })
         assert.equal(result, error_string)
     });
 
-    it('valid inputs return with non-nil target field value', function() {
-        let report= new ReportParser
-        
-        let result = report.parse(parser_config, (err, value)=> {console.log(value)})
+    it('valid inputs return with non-nil target field value', function () {
+        let report = new ReportParser
 
-        assert.isNotNaN(result, "Result is not nil.")
+        let result = report.parse(parser_config_complete, (err, value) => {
+            console.log(value)
+        })
+
+        assert.isNotNaN(result, "OK")
     })
 });
 
 
-describe('Directory merge', function(){
-    it('nil inputs returns empty string', function(){
+describe('Directory merge', function () {
+    it('nil inputs returns empty string', function () {
         let parser = new ReportParser
-        let result = parser.mergePath("","")
-        assert.equal( result, "")
+        let result = parser.mergePath("", "")
+        assert.equal(result, "")
     });
 
-    it('valid inputs concats strings', function(){
+    it('valid inputs concats strings', function () {
         let parser = new ReportParser
         let result = parser.mergePath("/test", "sample.report")
         assert.equal(result, "./test/sample.report")
     })
 })
 
-describe('Remove file after parse', function(){
-    it('remove_after_parse is true, return non-false ', async() =>{
+describe('Remove file after parse', function () {
+    it('remove_after_parse is true, return non-false ', async () => {
         let parser = new ReportParser
-        
+
         //ensure file exists
         fs.writeFileSync('./test.txt', "hello world")
 
@@ -67,7 +79,7 @@ describe('Remove file after parse', function(){
         assert.notEqual(result, false)
     })
 
-    it('remove after_parse is false, return false', function(){
+    it('remove after_parse is false, return false', function () {
         let parser = new ReportParser
         let result = parser.removeFile(false, 'test.txt')
 
@@ -75,11 +87,11 @@ describe('Remove file after parse', function(){
     })
 })
 
-describe('Parse lines', function(){
-    it('parse line logs each line', async()=>{
+describe('Parse lines', function () {
+    it('parse line logs each line', async () => {
         let parser = new ReportParser
 
-        let value= await parser.parseLine("./test/assets/qa.report", "x")
-        assert.equal(value , "38525348573.230")
+        let value = await parser.parseLine("./test/assets/qa.report", "x")
+        assert.equal(value, "38525348573.230")
     })
 })
